8fb0bed2166de41edda65983492852be23609fe3
fredericodr <fredericodomrap@gmail.com>
2025-05-22 13:23:23 +0100

    changes made

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ def load_data(path):
     df = pd.read_csv(path)
     return df
 
+<<<<<<< HEAD
 def clean_data(df):
     df.columns = [col.lower() for col in df.columns]
     return df
 =======
 def something_else(df):
     return df.clean()
+>>>>>>> features
